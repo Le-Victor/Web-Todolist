a8b2301bb7c3fad124a6217b78822e7bcab73a29
琦琦 <qiqi@qiqideMacBook-Pro.local>
2020-04-22 02:44:26 +0800

    e2e测试返回所有Todo任务

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -13,4 +13,12 @@ describe('todo test', function () {
     it('should have correct title-1', async function() {          
         expect(await page.title()).to.eql('Todo List');
     })
+
+    it('should render all content correct', async function() {
+      let todoList = await page.waitFor('#root > div > div.ui.centered.relaxed.grid.container > div:nth-child(3) > div > div.row > div');
+      const expectContent0 = await page.evaluate(todoList => todoList.children[0].querySelector('div').textContent, todoList);
+      const expectContent1 = await page.evaluate(todoList => todoList.children[1].querySelector('div').textContent, todoList);
+      expect(expectContent0).to.eql('Restful API homework');
+      expect(expectContent1).to.eql('update homework');
+    }) 
   });
