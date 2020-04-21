d3c984c926010307bdbb02ca2f35a54bb6381208
琦琦 <qiqi@qiqideMacBook-Pro.local>
2020-04-22 02:50:20 +0800

    e2e添加新的Todo任务

--- e2e/test/sample.spec.js
+++ e2e/test/sample.spec.js
@@ -21,4 +21,13 @@ describe('todo test', function () {
       expect(expectContent0).to.eql('Restful API homework');
       expect(expectContent1).to.eql('update homework');
     }) 
+
+    it('should new todo correct', async function() {
+      await page.click('#header_icon', {delay: 500});
+      await page.type('body > div.ui.page.modals.dimmer.transition.visible.active > div > div.content > div > input[type=text]', 'new todo item', {delay: 50});
+      await page.click('body > div.ui.page.modals.dimmer.transition.visible.active > div > div.actions > button.ui.icon.positive.right.labeled.button')
+      let todoList = await page.waitFor('#root > div > div.ui.centered.relaxed.grid.container > div:nth-child(3) > div > div.row > div');
+      const expectInputContent = await page.evaluate(todoList => todoList.lastChild.querySelector('div').textContent, todoList);
+      expect(expectInputContent).to.eql('new todo item');
+    }) 
   });
